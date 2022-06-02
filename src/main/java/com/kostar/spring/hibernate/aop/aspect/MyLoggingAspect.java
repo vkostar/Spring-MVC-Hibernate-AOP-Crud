794cecb22c3b7b993239398170ca7ed627e2eca4
”konstantin <vihroff@gmail.com>
2022-06-03 00:43:50 +0300

    The is finished

---
+++ src/main/java/com/kostar/spring/hibernate/aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,31 @@
+package com.kostar.spring.hibernate.aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+
+    @Around("execution(* com.kostar.spring.hibernate.aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targetMethodResult;
+    }
+
+
+}
